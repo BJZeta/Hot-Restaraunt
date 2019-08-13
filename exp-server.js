e634f065045983fd10c63a06c37604838854f123
Brandon U <unzueta.brandon@gmai.com>
2019-08-12 20:14:01 -0700

    adding more routes and html

--- exp-server.js
+++ exp-server.js
@@ -13,6 +13,7 @@ app.listen(PORT, function() {
 
   var reservations = [
     {
+      routeName: "PeterParker",
       id: "Spidey",
       name: "Peter Parker",
       email: "peter_b_parker@esu.com",
@@ -22,6 +23,7 @@ app.listen(PORT, function() {
 
   var waitList = [
     {
+      routeName: "ClarkKent",
       id: "Not Supes",
       name: "Clark Kent",
       email: "kent.clark@dailyplanet.com",
@@ -31,4 +33,32 @@ app.listen(PORT, function() {
 
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+  });
+
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+  app.get("/api/tables", function(req, res) {
+    return res.json(reservations);
+  });
+
+  app.get("/api/waitinglist", function(req, res) {
+    return res.json(waitList);
+  });
+
+  app.get("/api/tables/:table", function (req,res) {
+    var chosen = req.params.table;
+    for (var i=0; i < reservations.length;i++) {
+      if(chosen === reservations[i].routeName) {
+        return res.json(reservations[i]);
+      }
+    }
+
+    return res.json(false);
+
   });
