7108cf4a5d75e850e4cc28b0a14af4deb4d413d5
Brandon U <unzueta.brandon@gmai.com>
2019-08-12 19:40:32 -0700

    adding first route and html to page

--- exp-server.js
+++ exp-server.js
@@ -10,3 +10,25 @@ app.use(express.json());
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
+  var reservations = [
+    {
+      id: "Spidey",
+      name: "Peter Parker",
+      email: "peter_b_parker@esu.com",
+      phone: "562-867-5309"
+    }
+  ];
+
+  var waitList = [
+    {
+      id: "Not Supes",
+      name: "Clark Kent",
+      email: "kent.clark@dailyplanet.com",
+      phone: "573-847-1111"
+    }
+  ];
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
